85e81c440ba2aa23d735457f929c047beef6b750
Marco <marcobaruco2091@gmail.com>
2025-02-06 16:56:01 -0300

    fim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent ],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
